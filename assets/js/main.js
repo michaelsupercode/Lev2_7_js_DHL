20de11e2b361ca48cca8442409ed869249dc1bc6
SuperCoder <supercoder@MacBook-Air-von-SuperCoder-3.fritz.box>
2022-02-10 22:24:30 +0100

    firstcom

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+let mylist = document.getElementById('mylist')
+let masse = document.getElementById('masse')
+
+
+function showtxt() {
+
+    let trueMylist = Number(mylist.value)
+
+    switch (true) {
+
+        case trueMylist === 0:
+            masse.innerHTML = ` <b>Brief und Postkarte</b> <br>
+            L: 10 - 23,5 cm
+            B: 7 - 12,5 cm
+            H: bis 1 cm`
+            break;
+
+        case trueMylist === 1:
+            masse.innerHTML = `<b>DHL Paket 2 kg</b> <br>
+            bis 60 x 30 x 15 cm`
+            break;
+
+        case trueMylist === 2:
+            masse.innerHTML = `<b>DHL Paket 5 kg</b> <br>
+            bis 120 x 60 x 60 cm`
+            break;
+
+        case trueMylist === 3:
+            masse.innerHTML = `<b>DHL Paket 10 kg</b> <br>
+            bis 120 x 60 x 60 cm`
+            break;
+
+        default:
+            masse.innerHTML = `<b>Extra große Größe</b>`
+            break;
+    }
+}
